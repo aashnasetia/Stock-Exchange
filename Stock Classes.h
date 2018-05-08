a2c578c1e20be9733dd104b0fe08e1cc5c5a24a9
Chris Mangkuorahardjo <cm6652@gmail.com>
2018-05-08 14:29:28 -0700

    Added functions

--- Stock Classes.h
+++ Stock Classes.h
@@ -6,31 +6,97 @@ class Company
 	BST* sellList; // Min sorted heap
 	
 	public:
-	void findData (std::string a)
-	void pullData (BST* &a, Customer &b)
-	void pushData (BST* &a, Customer b)
+	void findData (std::string a);
+	void minSort (BST* &a, Customer b);
+	void maxSort (BST* &a, Customer b);
+	void pullData (BST* &a, Customer &b);
+	void pushData (BST* &a, Customer b);
+	void Create(std::string n)
+	{
+		name = n;
+		buyList* = new BST;
+		sellList* = new BST;
+	}
 };
 
 
 
 class Customer 
 {
+	private:
 	std::string name;
+	std::string pass;
 	std::int quantity;
 	std::double price;
-	std::string BuyOrSell;
+	std::int BuyOrSell; // 0 for buy, 1 for sell
+	
+	public:
+	std::string getName() {return name;}
+	std::string getPass() {return pass;}
+	std::int getQuantity() {return quantity;}
+	std::double getPrice() {return price;}
+	std::string getType() {return BuyOrSell;}
+	
+	// Add input validation to setQuantity, and setPrice
+	void setName(std::string a) 
+	{
+		name = a;
+	}
+	void setQuantity(std::int a)
+	{
+		quantity = a;
+	}
+	void setPrice(std::double a) 
+	{
+		price = a;
+	}
+	void setType(std::int a)
+	{
+		while ((!a)||(a<0)||(a>1))
+		{
+			if(std::cin.fail())
+			{
+				std::cin.clear();
+				std::cin.ignore(numeric_limits<streamsize>::max(),'\n');
+			}
+			std::cout << "Please enter a valid value (0: Buy, 1: Sell) : ";
+			std::cin >> a;
+			cin.ignore();
+		}
+		BuyOrSell = a;
+	}	
 };
 
 
 
-struct BST*
+class BST*
 {
+	private:
 	Customer a;
 	BST* left;
 	BST* right;
 	
+	public:
+	void minSort (BST* &a, Customer b);
+	void maxSort (BST* &a, Customer b);
+	void Insert (BST* &a, Customer b);
+	bool Find (BST* a, Customer b);
 };
 
+// Input validation code, replace parameters with relevant values
+/* 
+	while ((!a)||(a<0)||(a>1))
+		{
+			if(std::cin.fail())
+			{
+				std::cin.clear();
+				std::cin.ignore(numeric_limits<streamsize>::max(),'\n');
+			}
+			std::cout << "Please enter a valid value (0: Buy, 1: Sell) : ";
+			std::cin >> a;
+			cin.ignore();
+		}
+*/
 
 
 // Store companies in a vector
